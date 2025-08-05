1b195553f8996a06d77fe68aad2f13e6e2c2d99d
IIESA <jquevedoo@iiesa-solutions.com.mx>
2025-08-05 12:48:07 -0600

    Primer commit del proyecto Django

--- Gestion/urls.py
+++ Gestion/urls.py
@@ -1,24 +1,8 @@
-"""
-URL configuration for Gestion project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  # <- ¡aquí está el fix!
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Instrumentos.urls')),  # Incluye las URLs de la app Instrumentos
-    path('gasolina/', include('Gasolina.urls')),  # Incluye las URLs de la app Gasolina
+    path('', include('Instrumentos.urls')),  # rutas de tu app principal
+    path('gasolina/', include('Gasolina.urls')),  # rutas de la app de gasolina
 ]
