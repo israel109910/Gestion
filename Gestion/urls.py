f2c8d2a26bcd20df244f3efac26a2299304237b2
IIESA <jquevedoo@iiesa-solutions.com.mx>
2025-08-06 11:33:34 -0600

    insertar app gasolina

--- Gestion/urls.py
+++ Gestion/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from Instrumentos import views as instrumentos_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', instrumentos_views.inicio, name='home'),  # Página principal
     path('instrumentos/', include(('Instrumentos.urls', 'instrumentos'), namespace='instrumentos')),
     path('gasolina/', include(('Gasolina.urls', 'gasolina'), namespace='gasolina')),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
